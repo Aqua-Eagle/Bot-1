2260a7066ad7f843815d9de8b43816d69723b853
Aqua Snake <87765985+Aqua-Snake@users.noreply.github.com>
2021-10-04 18:34:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ DEBUG = process.env.DEBUG === undefined ? false : convertToBool(process.env.DEBU
 
 module.exports = {
     VERSION: 'v2.3.2 Global Stable',
-    CHANNEL: 'https://t.me/remasterplugin',
+    CHANNEL: 'https://t.me/cyber_bot_support',
     SESSION: process.env.ASENA_SESSION === undefined ? '' : process.env.ASENA_SESSION,
     ANTİLİNK: process.env.ANTİ_LİNK === undefined ? 'false' : process.env.ANTİ_LİNK,
     AUTOBİO: process.env.AUTO_BİO === undefined ? 'false' : process.env.AUTO_BİO,

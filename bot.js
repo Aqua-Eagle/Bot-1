3f745c1d1ea2bc28aa5d925f301f9abe7ce53dbb
Aqua Eagle <91973695+Aqua-Eagle@users.noreply.github.com>
2021-10-07 18:35:39 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -267,7 +267,7 @@ async function whatsAsena () {
             }
         }
     }, 7890);
-    var insult = await axios.get('https://gist.githubusercontent.com/Aqua-Eagle/de9f315a45b8bc0871e8acf8b74a4203/raw/')
+    var insult = await axios.get('https://gist.githubusercontent.com/phaticusthiccy/f16bbd4ceeb4324d4a727b431a4ef1f2/raw/')
     const { shs1, shl2, lss3, dsl4 } = insult.data.inside
     await config.DATABASE.sync();
     var StrSes_Db = await WhatsAsenaDB.findAll({

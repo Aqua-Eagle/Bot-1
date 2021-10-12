154942a76c54318ad1af245de4e80d888792a291
Aqua Eagle <91973695+Aqua-Eagle@users.noreply.github.com>
2021-10-11 23:54:35 -0700

    Update ip.js

--- plugins/ip.js
+++ plugins/ip.js
@@ -1,3 +1,6 @@
+/*Coded by Aqua Snake */
+
+
 const Asena = require('../events');
 const Config = require('../config');
 const {MessageType} = require('@adiwajshing/baileys');
@@ -21,9 +24,9 @@ const PROXY = "PROXY :"
 const NOT_FOUNDIP = "```Sorry,I could not your IP 😖```"
 
 
+if (Config.WORKTYPE == 'private') {
 
-
-Asena.addCommand({pattern: 'ip ?(.*)', desc: 'gives you the detail of your IP' ,fromMe: false}, async (message, match) => {
+ Asena.addCommand({pattern: 'ip ?(.*)', desc: 'gives you the detail of your IP' ,fromMe: false}, async (message, match) => {
     
     if (match[1] === '') return await message.reply(NEED_IP);
 	const url = `https://api.techniknews.net/ipgeo/${match[1]}`;
@@ -47,3 +50,33 @@ Asena.addCommand({pattern: 'ip ?(.*)', desc: 'gives you the detail of your IP' ,
 		return await message.client.sendMessage(message.jid, NOT_FOUNDIP, MessageType.text);
 	}
  });
+ 
+}
+
+else if (Config.WORKTYPE == 'public') {
+    Asena.addCommand({pattern: 'ip ?(.*)', desc: 'gives you the detail of your IP' ,fromMe: false}, async (message, match) => {
+    
+    if (match[1] === '') return await message.reply(NEED_IP);
+    const url = `https://api.techniknews.net/ipgeo/${match[1]}`;
+    try {
+        const response = await got(url);
+        const ipjson = JSON.parse(response.body);
+        if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '*🔴 ' + IP +'* ```' + match[1] + '```\n\n' +
+        '*✅' + ST +'* ```' + ipjson.status+ '```\n' +
+        '*🌐' + CONTINENT +'* ```' + ipjson.continent+ '```\n' +
+        '*🗺' + COUNTRY +'* ```' + ipjson.country+ '```\n' +
+        '*🔢' + COUNTRYCODE +'* ```' + ipjson.countryCode+ '```\n' +
+        '*🌍' + REGIONNAME +'* ```' + ipjson.regionName+ '```\n' +
+        '*🚩' + CITY +'* ```' + ipjson.city+ '```\n' +
+        '*🏛' + ZIP +'* ```' + ipjson.zip+ '```\n' +
+        '*💸' + CURRENCY +'* ```' + ipjson.currency+ '```\n\n' +
+        '*📡' + ISP +'* ```' + ipjson.isp+ '```\n' +
+        '*🛡' + PROXY +'* ```' + ipjson.proxy+ '```\n' +
+        '*📱' + MOBILE +'* ```' + ipjson.mobile+ '```\n', MessageType.text);
+    } 
+    catch {
+        return await message.client.sendMessage(message.jid, NOT_FOUNDIP, MessageType.text);
+    }
+ });
+
+}

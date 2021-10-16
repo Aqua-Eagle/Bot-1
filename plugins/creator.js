6402202f4ca89f94441fb08b2f815433c6016c44
Aqua Snake <87765985+Aqua-Snake@users.noreply.github.com>
2021-10-16 12:55:12 -0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -6,7 +6,7 @@ const Config = require('../config');
 
 if (Config.WORKTYPE == 'private') {
 
-Asena.addCommand({pattern: 'creater', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+Asena.addCommand({pattern: 'creator', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
 
 
 	 await message.sendMessage(fs.readFileSync('media/CyberBot.jpg'), MessageType.image, {caption: `_____ *Cyber Bot* _____\n\n Creator : Aqua Snake \n Github : https://github.com/Aqua-Snake/ \n Whatsapp : wa.me/+94764746599 \n Bot Link : https://aqua-snake.github.io/CBot/ `});

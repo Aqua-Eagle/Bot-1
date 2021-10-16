4bb55ee24625c8a2c9a0216d7250a42d1d4e6b4b
Aqua Snake <87765985+Aqua-Snake@users.noreply.github.com>
2021-10-16 13:00:35 -0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -6,10 +6,10 @@ const Config = require('../config');
 
 if (Config.WORKTYPE == 'private') {
 
-Asena.addCommand({pattern: 'creator', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+Asena.addCommand({pattern: 'creater', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
 
 
-	 await message.sendMessage(fs.readFileSync('media/CyberBot.jpg'), MessageType.image, {caption: `_____ *Cyber Bot* _____\n\n Creator : Aqua Snake \n Github : https://github.com/Aqua-Snake/ \n Whatsapp : wa.me/+94764746599 \n Bot Link : https://aqua-snake.github.io/CBot/ `});
+	 await message.sendMessage(fs.readFileSync('media/CyberBot.jpg'), MessageType.image, {caption: `_____ *Cyber Bot* _____\n\n Creator : Aqua Snake \n Github : https://github.com/Aqua-Snake/ \n Whatsapp : wa.me/+94764746599 \n Telegram : https://t.me/Aqua_Smake/ \n Bot Link : https://aqua-snake.github.io/CBot/ `});
 
 	}));
 }
@@ -18,6 +18,6 @@ else if (Config.WORKTYPE == 'public') {
 
 Asena.addCommand({pattern: 'creator', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
 
-	    await message.sendMessage(fs.readFileSync('media/CyberBot.jpg'), MessageType.image, {caption: `_____ *Cyber Bot* _____\n\n Creator : Aqua Snake \n Github : https://github.com/Aqua-Snake/ \n Whatsapp : wa.me/+94764746599 \n Bot Link : https://aqua-snake.github.io/CBot/ `});
+	    await message.sendMessage(fs.readFileSync('media/CyberBot.jpg'), MessageType.image, {caption: `_____ *Cyber Bot* _____\n\n Creator : Aqua Snake \n Github : https://github.com/Aqua-Snake/ \n Whatsapp : wa.me/+94764746599 \n Telegram : https://t.me/Aqua_Smake/ \n Bot Link : https://aqua-snake.github.io/CBot/ `});
     }));
 }
